dd7be44cbfc2e9471bdf0d2794dfd0b8a394a832
GLucky31 <59827647+GLucky31@users.noreply.github.com>
2020-01-14 12:51:14 +0100

    Add files via upload

---
+++ funkcije.py
@@ -0,0 +1,59 @@
+#Funkcija za korenjanje
+
+##def sqrfunc(x):
+##    print(x**2)
+##a = int(input("Vnesi število  za kvadriranje:"))
+##sqrfunc(a)
+
+#Zamenjevalec vrednosti
+
+##def valueSwapper():
+##    i = int(input("1"))
+##    j = int(input("2"))
+##    a = i
+##    i = j
+##    j = a
+##    print("1. je:", i,"2. je", j)
+##
+##valueSwapper()
+
+#LihoChecker
+
+##a = int(input("Liho ali ne:"))
+##
+##def lihoChecker(x):
+##    f = x%2
+##    if(f>0):
+##        print("Liho število.")
+##    else:
+##        print("Ni liho število.")
+##
+##lihoChecker(a)
+
+#Pra število check
+
+##st = int(input("Enter number:"))
+##
+##def primeChecker(num):
+##    if(num>1):
+##        for i in range(2,num):
+##            if((num%i) == 0):
+##                print("Število ni praštevilo:")
+##                break
+##            else:
+##                print("Število je praštevilo.")
+##                break
+##
+##primeChecker(st)
+##
+
+#Največji v listu
+
+##list1 = [10, 20, 30, 50, 5, 6, 20]
+##
+##def sorter(x):
+##    print("Največjo št. v listu je", max(x))
+##
+##sorter(list1)
+##
+
---
+++ neki.py
@@ -0,0 +1,134 @@
+##a = int(input("Vnesi vrednost a:"))
+##b = int(input("Vnesi vrednost b:"))
+##
+##vsota = a + b
+##vsota1 = a + b + 2
+##if (vsota == 10):
+##    print("Enako 10 je. ;DDDD")
+##elif(vsota == 8):
+##    print("Enako 8 je???? :3333333")
+##else:
+##    print("Števila se ne seštejejo v 10 ali 8. ;000000000")
+##
+##if(vsota1 == 102 and a == 49 and b == 51):
+##    print("Secret ending unlocked.")
+##else:
+##    print("No secret ending.")
+
+##navodilo = '''UPORABNIK NA VNESE 2 SPREMENLJIVKI. PRVA NAJE SHRANI VNOS2 NA VNOS1.
+##ČE JE SPREMENLJIVKA 1 ENAKA SPREMENLJIVKI2 IZPIŠI ENAKO IN SPREMENLJIVKE. ČE JE
+##SPREMENLJIVKA1 ENAKA 2 IZPIŠI ENAKO 2. IN ČE NE USTREZA POHOJU IZPIŠI ERORCIC'''
+##
+##print(navodilo)
+##
+##a = int(input("Vnos1:"))
+##b = int(input("Vnos2:"))
+##b = b**a
+##
+##if(a == b):
+##    print("enako", b, "^", a)
+##else:
+##    print("ERORCIC")
+##
+##
+##from random import *
+##while(True):
+##    a = randint(1, 2)
+##b = int(input("Ugani št. od 1 do 2:"))
+##
+##if(a == b):
+##    print("Bravo!")
+##else:
+##    print("Napačno!")
+
+##a = int(input("Vnesi število:"))
+##b = int(a%2)
+##a = a/2
+##c = int(a%2)
+##a = a/2
+##d = int(a%2)
+##a = a/2
+##e = int(a%2)
+##a = a/2
+##f = int(a%2)
+##a = a/2
+##g = int(a%2)
+##a = a/2
+##h = int(a%2)
+##a = a/2
+##i = int(a%2)
+##a = a/2
+##
+##print(i, h, g, f, e, d, c, b)
+
+##a = 1
+##b = 5
+##while(a<b):
+##    print(a)
+##    a+=1
+##    if(a>b):
+##        break
+##    
+
+##a = char(input("znak vpiši:"))
+##
+##while(True):
+##    if(a==char(k)):
+##        break
+
+##ok = "Oblivion npc gay fgt"
+##i = int(2)
+
+##a= int(input("Wow"))
+##
+##for i in range(1,11):
+##    if(i==11):
+##        break
+##    print(a, "x", i, "=", a*i)
+
+##n = int(input("Your mother man:"))
+##
+##for i in range(0,n):
+##    print("*"* (1+i))
+
+##size = int(input("Size:"))
+##m = (2 * size) - 2
+##for i in range(0, size):
+##    for j in range(0, m):
+##        print(end=" ")
+##    m = m - 1 
+##    for j in range(0, i + 1):
+##        print("*", end=' ')
+##    print(" ")
+##
+
+##list1 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+##a=int(input("Število črke:"))
+##
+##while(a>0):
+##    print(list1[a-1])
+##    a=int(input("Število črke:"))
+##    if(a==0):
+##        break
+##if(a==0):
+##    print("The amount of people that love you is", a)
+
+
+##from random import *
+##b=int(input("Enter input:"))
+##
+##while(b!=0):
+##    if(b>0):
+##        a=randint(1,20)
+##    print(a)
+##    b=int(input("Enter input:"))
+##    if(b==0):
+##        print("Stopping program..")
+##        break
+
+gay = 100
+mum = gay
+
+if(gay==mum):
+    while(mum==gay):
+        print("Bruh true?")
